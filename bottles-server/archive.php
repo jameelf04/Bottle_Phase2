d5f7c180672f3edd77482028885f1a35060cd1c5
Jameel Fakhoury <jameel.fakhoury74@gmail.com>
2026-08-05 01:27:16 +0300

    Add archive.php - public endpoint for archived bottles

---
+++ bottles-server/archive.php
@@ -0,0 +1,38 @@
+<?php
+include(__DIR__ . "/database/connection.php");
+
+$sql = "SELECT b.bottleid, b.message, b.time,
+        (SELECT COUNT(*) FROM marks m WHERE m.bottleid = b.bottleid) AS markcount
+        FROM bottles b
+        WHERE b.status = 'archived'
+        ORDER BY b.time DESC";
+$query = $mysql->prepare($sql);
+$query->execute();
+$array = $query->get_result();
+
+$response = [];
+$response["success"] = true;
+$response["data"] = [];
+
+while ($bottle = $array->fetch_assoc()){
+    $bottleData = [];
+    $bottleData["bottleid"] = $bottle["bottleid"];
+    $bottleData["message"] = $bottle["message"];
+    $bottleData["markcount"] = $bottle["markcount"];
+
+    $sql2 = "SELECT content FROM marks WHERE bottleid = ? ORDER BY time ASC";
+    $query2 = $mysql->prepare($sql2);
+    $query2->bind_param("i", $bottle["bottleid"]);
+    $query2->execute();
+    $array2 = $query2->get_result();
+
+    $bottleData["marks"] = [];
+    while ($mark = $array2->fetch_assoc()){
+        $bottleData["marks"][] = $mark["content"];
+    }
+
+    $response["data"][] = $bottleData;
+}
+
+echo json_encode($response);
+?>
